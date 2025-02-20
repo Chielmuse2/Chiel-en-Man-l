a468dd213dab5c15fc8a5674636fbd66642e61ac
ChielMuse <114626@montessoricollege.nl>
2025-02-20 13:18:59 +0100

    half 4.7.3

--- MCPizzeriaGUI.py
+++ MCPizzeriaGUI.py
@@ -16,7 +16,18 @@ import MCPizzeriaSQL
 
 
 ### ---------  Functie definities  -----------------
-
+def zoekKlant():
+ #haal de ingevoerde_klantnaam op uit het invoerveld
+ # en gebruik dit om met SQL de klant in database te vinden
+ gevonden_klanten = MCPizzeriaSQL.zoekKlantInTabel(invoerveldKlantnaam.get())
+ print(gevonden_klanten) # om te testen
+ invoerveldKlantnaam.delete(0, END) #invoerveld voor naam leeg maken
+ invoerveldKlantNr.delete(0, END) #invoerveld voor klantNr leeg maken
+ for rij in gevonden_klanten: #voor elke rij dat de query oplevert
+    #toon klantnummer, de eerste kolom uit het resultaat in de invoerveld
+    invoerveldKlantNr.insert(END, rij[0]) 
+    #toon klantAchternaam, de tweede kolom uit het resultaat in de invoerveld
+    invoerveldKlantnaam.insert(END, rij[1]) 
 
 ### --------- Hoofdprogramma  ---------------
 
@@ -29,5 +40,26 @@ closeWindow.grid(row=17,column=4)
 labelIntro = Label(window, text="HALLOOOOOOOO!")
 labelIntro.grid(row=0, column=0, sticky="W")
 
+labelIntro = Label(window, text="Klantnaam:")
+labelIntro.grid(row=1, column=0, sticky="W")
+ingevoerde_klantnaam = StringVar()
+invoerveldKlantnaam = Entry(window, textvariable=ingevoerde_klantnaam)
+invoerveldKlantnaam.grid(row=1, column=1, sticky="W")
+knopzoekopklantnaam = Button(window, text="Zoek klant", width=12, command=zoekKlant)
+knopzoekopklantnaam.grid(row=1, column=4)
+
+labelIntro = Label(window, text="Klantnummer:")
+labelIntro.grid(row=2, column=0, sticky="W")
+invoerveldKlantNr = Entry(window)
+invoerveldKlantNr.grid(row=2, column=1, sticky="W")
+
+labelIntro = Label(window, text="pizzanaam:")
+labelIntro.grid(row=3, column=0, sticky="W")
+ingevoerde_pizzanaam = StringVar()
+invoerveldpizzanaam = Entry(window, textvariable=ingevoerde_pizzanaam)
+invoerveldpizzanaam.grid(row=3, column=1, sticky="W")
+
+labelIntro = Label(window, text="Mogelijkheden:")
+labelIntro.grid(row=4, column=0, sticky="W")
 #reageert op gebruikersinvoer, deze regel als laatste laten staan
 window.mainloop()
