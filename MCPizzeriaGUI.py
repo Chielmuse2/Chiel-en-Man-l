562464ce3c6bee8f6729c9d114b52a2076797a19
ChielMuse <114626@montessoricollege.nl>
2025-02-20 12:39:07 +0100

    4.7.2

--- MCPizzeriaGUI.py
+++ MCPizzeriaGUI.py
@@ -23,10 +23,11 @@ import MCPizzeriaSQL
 window = Tk()
 window.iconbitmap("MC_icon.ico") #Let op: Dit werkt niet op een MAC! Zet deze regel dan in commentaar
 window.wm_title("Gedverderriedezepizasmaaktnaardrerrie Pizzeria")
-venster = Tk()
 
-closeWindow = Button(window, text="Close", width=50, height=2, command=venster.)
-closeWindow.grid(row=0,column=0)
+closeWindow = Button(window, text="Close", width=50, height=2, command=window.destroy)
+closeWindow.grid(row=17,column=4)
+labelIntro = Label(window, text="HALLOOOOOOOO!")
+labelIntro.grid(row=0, column=0, sticky="W")
 
 #reageert op gebruikersinvoer, deze regel als laatste laten staan
 window.mainloop()
