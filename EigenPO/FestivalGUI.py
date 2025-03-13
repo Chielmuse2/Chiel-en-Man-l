81476b9dcb09a7f0076f7f1ee7692874dd6cbd44
Chielmuse <114626@montessoricollege.nl>
2025-03-13 12:22:03 +0100

    awdaw

--- EigenPO/FestivalGUI.py
+++ EigenPO/FestivalGUI.py
@@ -1,2 +1,5 @@
+from tkinter import *
+import MCPizzeriaSQL
 ################## def's ##################
+
 ################## Hoofdprogramma ##################
--- EigenPO/FestivalSQL.py
+++ EigenPO/FestivalSQL.py
@@ -1,6 +1,18 @@
 # hey renske dit het PO van Manèl en Chiel
 
-
+import sqlite3
+with sqlite3.connect("MCPizzeria.db") as db:
+    cursor = db.cursor()#cursor is object waarmee je data uit de database kan halen
 
 ################## def's ##################
+
+def maakTabellenAan():
+     # Maak een nieuwe tabel met 3 kolommen: id, naam, prijs
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tbl_klanten(
+            gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
+            gerechtNaam TEXT NOT NULL,
+            gerechtPrijs REAL NOT NULL);""")
+    
+    print("Tabel 'tbl_klanten' aangemaakt.")
 ################## Hoofdprogramma ##################
