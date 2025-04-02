dfbce4e2e4794703c64c51262b3e89916e0fa8a3
chiel <114626@montessoricollege.nl>
2025-04-02 15:30:42 +0200

    yeassss

--- EigenPO/FestivalGUI.py
+++ EigenPO/FestivalGUI.py
@@ -1,2 +1,22 @@
+# hey renske dit het PO van Manèl en Chiel
+
+from tkinter import *
+import MCPizzeriaSQL
+
+
 ################## def's ##################
+
+
 ################## Hoofdprogramma ##################
+
+window = Tk()
+window.wm_title("Ponkpip Festival :D")
+
+
+
+
+
+
+
+#reageert op gebruikersinvoer, deze regel als laatste laten staan
+window.mainloop()
--- EigenPO/FestivalSQL.py
+++ EigenPO/FestivalSQL.py
@@ -1,6 +1,19 @@
 # hey renske dit het PO van Manèl en Chiel
 
-
+import sqlite3
+with sqlite3.connect("Festival.db") as db:
+    cursor = db.cursor()#cursor is object waarmee je data uit de database kan halen
 
 ################## def's ##################
+
+def maaktabel():
+    cursor.execute("CREATE TABLE IF NOT EXISTS tbl_consument (gegvensID INTEGER PRIMARY KEY AUTOINCREMENT, consumentID INTERGER NOT NULL, programmaID INTERGER NOT NULL, dag_kamping INTERGER, parkeerplek INTERGER)")
+    print("tabel 'tbl_consument' aangemaakt")
+
+def printtabel(tabel_naam):
+ cursor.execute("SELECT * FROM " + tabel_naam) #SQL om ALLE gegevens te halen
+ opgehaalde_gegevens = cursor.fetchall() #sla gegevens op in een variabele
+ print("Tabel " + tabel_naam + ":", opgehaalde_gegevens) #druk gegevens af
 ################## Hoofdprogramma ##################
+maaktabel()
+printtabel("tbl_consument")
